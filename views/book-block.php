36cec1d6ae867994cba0e70f4ba6c927fbdea7a3
roman <roman.kr43@gmail.com>
2024-04-27 22:02:04 +0200

    Added view for a book block in the online store.

---
+++ views/book-block.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../pages/resources/css/axure_rp_page.css" type="text/css" rel="stylesheet"/>
+    <link href="../pages/data/styles.css" type="text/css" rel="stylesheet"/>
+    <link href="book-block.css" type="text/css" rel="stylesheet"/>
+    <title>Shop List</title>
+</head>
+<body>
+    <!-- block for a book -->
+    <div id="book_block" class="">
+
+        <!-- image for the book -->
+        <img id="book_image" class="img " src="../pages/images/shop_list/u1.svg">
+
+        <!-- book details -->
+        <div id="book_details_div">
+            <!-- Book title -->
+            <div id="book_title" class="text ">
+                    <p><span>"Я (Романтика)"</span></p>
+            </div>
+
+            <!-- author -->
+            <div id="book_author" class="text ">
+                <p><span>Микола Хвильовий</span></p>
+            </div>
+
+            <!-- separator line-->
+            <img id="separator_line" class="img " src="../pages/images/shop_list/u52.svg">
+
+            <!-- price tag-->
+            <div id="price_tag" class="text ">
+                <p><span>485 грн</span></p>
+            </div>
+        </div>
+
+        <!-- button for placing an order-->
+        <div id="book_order_div">
+            <img id="book_order_btn" class="img " src="../pages/images/shop_list/u43.svg">
+            <div id="book_order_text" class="text ">
+                <p><span>У кошик</span></p>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
