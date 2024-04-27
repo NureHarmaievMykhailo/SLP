bf62f8d724dfd38f1b3ae90d642df8dbd7e39407
roman <roman.kr43@gmail.com>
2024-04-28 01:18:16 +0200

    Implemented shop_list rendering. Added sample data to the shop_list view.

--- views/shop_list.php
+++ views/shop_list.php
@@ -47,7 +47,85 @@
   </head>
   <body>
     <div id="base" class="">
+      <header>
+        <div id="u9" class="ax_default">
+          <img id="u9_menu" class="img " src="../pages/images/shop_list/u9_menu.png" alt="u9_menu"/>
 
+          <!-- Unnamed (Table) -->
+          <div id="u10" class="ax_default">
+
+            <!-- Unnamed (Menu Item) -->
+            <div id="u11" class="ax_default menu_item">
+              <img id="u11_img" class="img " src="../pages/images/shop_list/u11.png"/>
+              <div id="u11_text" class="text " style="display:none; visibility: hidden">
+                <p></p>
+              </div>
+            </div>
+            <!-- Unnamed (Menu Item) -->
+            <div id="u12" class="ax_default menu_item">
+              <img id="u12_img" class="img " src="/pages/images/shop_list/u12.png"/>
+              <div id="u12_text" class="text ">
+                <p><span>Головна сторінка</span></p>
+              </div>
+            </div>
+
+            <!-- Unnamed (Menu Item) -->
+            <div id="u13" class="ax_default menu_item">
+              <img id="u13_img" class="img " src="/pages/images/shop_list/u13.png"/>
+              <div id="u13_text" class="text ">
+                <p><span>Навчальні матеріали</span></p>
+              </div>
+            </div>
+
+            <!-- Unnamed (Menu Item) -->
+            <div id="u14" class="ax_default menu_item">
+              <img id="u14_img" class="img " src="/pages/images/shop_list/u14.png"/>
+              <div id="u14_text" class="text ">
+                <p><span>Викладачі</span></p>
+              </div>
+            </div>
+
+            <!-- Unnamed (Menu Item) -->
+            <div id="u15" class="ax_default menu_item">
+              <img id="u15_img" class="img " src="/pages/images/shop_list/u15.png"/>
+              <div id="u15_text" class="text ">
+                <p><span>Книжковий магазин</span></p>
+              </div>
+            </div>
+
+            <!-- Unnamed (Menu Item) -->
+            <div id="u16" class="ax_default menu_item">
+              <img id="u16_img" class="img " src="/pages/images/shop_list/u16.png"/>
+              <div id="u16_text" class="text ">
+                <p><span>Мій кошик</span></p>
+              </div>
+            </div>
+            <!-- Logo header -->
+            <div id="u54" class="ax_default button">
+              <img id="u54_img" class="img " src="/pages/images/shop_list/u54.svg"/>
+              <div id="u54_text" class="text ">
+                <p><span>Fluent<br>Ukrainian</span></p>
+              </div>
+            </div>
+
+            <!-- Logo icon -->
+            <div id="u55" class="ax_default image">
+              <img id="u55_img" class="img " src="/pages/images/shop_list/u55.png"/>
+              <div id="u55_text" class="text " style="display:none; visibility: hidden">
+                <p></p>
+              </div>
+            </div>
+
+            <!-- Cart menu option -->
+            <div id="u89" class="ax_default icon">
+              <img id="u89_img" class="img " src="/pages/images/shop_list/u89.svg"/>
+              <div id="u89_text" class="text " style="display:none; visibility: hidden">
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
       <!-- Header line -->
       <div id="u7" class="ax_default line2">
         <img id="u7_img" class="img " src="../pages/images/shop_list/u7.svg"/>
@@ -56,253 +134,199 @@
         </div>
       </div>
 
-      <!-- Unnamed (Menu) -->
-      <div id="u9" class="ax_default">
-        <img id="u9_menu" class="img " src="../pages/images/shop_list/u9_menu.png" alt="u9_menu"/>
-
-        <!-- Unnamed (Table) -->
-        <div id="u10" class="ax_default">
-
-          <!-- Unnamed (Menu Item) -->
-          <div id="u11" class="ax_default menu_item">
-            <img id="u11_img" class="img " src="../pages/images/shop_list/u11.png"/>
-            <div id="u11_text" class="text " style="display:none; visibility: hidden">
-              <p></p>
-            </div>
-          </div>
-          <?php
-            $book_title = "\"Я (Романтика)\"";
-            $book_author = "Микола Хвильовий";
-            $book_price = 485;
-            $book_image_uri = "/pages/images/shop_list/u1.svg";
-            $offset = "top:220px;left:100px;";
-            include('book-block.php');
-          ?>
-          <!-- Unnamed (Menu Item) -->
-          <div id="u12" class="ax_default menu_item">
-            <img id="u12_img" class="img " src="/pages/images/shop_list/u12.png"/>
-            <div id="u12_text" class="text ">
-              <p><span>Головна сторінка</span></p>
-            </div>
-          </div>
-
-          <!-- Unnamed (Menu Item) -->
-          <div id="u13" class="ax_default menu_item">
-            <img id="u13_img" class="img " src="/pages/images/shop_list/u13.png"/>
-            <div id="u13_text" class="text ">
-              <p><span>Навчальні матеріали</span></p>
-            </div>
-          </div>
-
-          <!-- Unnamed (Menu Item) -->
-          <div id="u14" class="ax_default menu_item">
-            <img id="u14_img" class="img " src="/pages/images/shop_list/u14.png"/>
-            <div id="u14_text" class="text ">
-              <p><span>Викладачі</span></p>
-            </div>
-          </div>
-
-          <!-- Unnamed (Menu Item) -->
-          <div id="u15" class="ax_default menu_item">
-            <img id="u15_img" class="img " src="/pages/images/shop_list/u15.png"/>
-            <div id="u15_text" class="text ">
-              <p><span>Книжковий магазин</span></p>
-            </div>
-          </div>
-
-          <!-- Unnamed (Menu Item) -->
-          <div id="u16" class="ax_default menu_item">
-            <img id="u16_img" class="img " src="/pages/images/shop_list/u16.png"/>
-            <div id="u16_text" class="text ">
-              <p><span>Мій кошик</span></p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u21" class="ax_default box_2">
-        <div id="u21_div" class=""></div>
-        <div id="u21_text" class="text " style="display:none; visibility: hidden">
-          <p></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u22" class="ax_default heading_2">
-        <div id="u22_div" class=""></div>
-        <div id="u22_text" class="text ">
-          <p><span>Про FluentUkrainian</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u23" class="ax_default heading_2">
-        <div id="u23_div" class=""></div>
-        <div id="u23_text" class="text ">
-          <p><span>Соціальні мережі</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u24" class="ax_default heading_2">
-        <div id="u24_div" class=""></div>
-        <div id="u24_text" class="text ">
-          <p><span>Застосунок</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Line) -->
-      <div id="u25" class="ax_default line1">
-        <img id="u25_img" class="img " src="/pages/images/shop_list/u25.svg"/>
-        <div id="u25_text" class="text " style="display:none; visibility: hidden">
-          <p></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u26" class="ax_default heading_2">
-        <div id="u26_div" class=""></div>
-        <div id="u26_text" class="text ">
-          <p><span>Підтримка</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u27" class="ax_default label">
-        <div id="u27_div" class=""></div>
-        <div id="u27_text" class="text ">
-          <p><span>© 2024 FluentUkrainian. Всі права захищені.</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Shape) -->
-      <div id="u28" class="ax_default icon">
-        <img id="u28_img" class="img " src="/pages/images/shop_list/u28.svg"/>
-        <div id="u28_text" class="text " style="display:none; visibility: hidden">
-          <p></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Shape) -->
-      <div id="u29" class="ax_default icon">
-        <img id="u29_img" class="img " src="/pages/images/shop_list/u29.svg"/>
-        <div id="u29_text" class="text " style="display:none; visibility: hidden">
-          <p></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Shape) -->
-      <div id="u30" class="ax_default icon">
-        <img id="u30_img" class="img " src="/pages/images/shop_list/u30.svg"/>
-        <div id="u30_text" class="text " style="display:none; visibility: hidden">
-          <p></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u31" class="ax_default label">
-        <div id="u31_div" class=""></div>
-        <div id="u31_text" class="text ">
-          <p><span>Facebook</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u32" class="ax_default label">
-        <div id="u32_div" class=""></div>
-        <div id="u32_text" class="text ">
-          <p><span>Instagram</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u33" class="ax_default label">
-        <div id="u33_div" class=""></div>
-        <div id="u33_text" class="text ">
-          <p><span>Telegram</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u34" class="ax_default label">
-        <div id="u34_div" class=""></div>
-        <div id="u34_text" class="text ">
-          <p><span>Освітній проєкт</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u35" class="ax_default label">
-        <div id="u35_div" class=""></div>
-        <div id="u35_text" class="text ">
-          <p><span>Підтримка</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u36" class="ax_default label">
-        <div id="u36_div" class=""></div>
-        <div id="u36_text" class="text ">
-          <p><span>Навчальні матеріали</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u37" class="ax_default label">
-        <div id="u37_div" class=""></div>
-        <div id="u37_text" class="text ">
-          <p><span>Питання та відповіді</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u38" class="ax_default label">
-        <div id="u38_div" class=""></div>
-        <div id="u38_text" class="text ">
-          <p><span>Детальніше</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
-      <div id="u39" class="ax_default label">
-        <div id="u39_div" class=""></div>
-        <div id="u39_text" class="text ">
-          <p><span>Викладачі</span></p>
-        </div>
-      </div>
-
-      <!-- Unnamed (Rectangle) -->
+      <!-- Page header -->
       <div id="u42" class="ax_default heading_1">
         <div id="u42_div" class=""></div>
         <div id="u42_text" class="text ">
           <p><span>Поринь у яскравий світ української літератури!</span></p>
         </div>
       </div>
+      <?php
+            require_once('../models/shopping-cart.php');
+            $romantyka = new Book("\"Я (Романтика)\"", 485, "", "/pages/images/shop_list/u1.svg", "Микола Хвильовий");
+            $kaydasheva_simya = new Book("\"Кайдашева сім'я\"", 500, "", "/pages/images/shop_list/u4.svg", "Іван Нечуй-Левицький");
+            $tygrolovy = new Book("\"Тигролови\"", 490, "", "/pages/images/shop_list/u6.svg", "Іван Багряний");
+            $trysta_poeziy = new Book("\"Триста поезій\"", 350, "", "/pages/images/shop_list/u59.svg", "Ліна Костенко");
+            $konotopska_vidma = new Book("\"Конотопська відьма\"", 400, "", "/pages/images/shop_list/u56.svg", "Грирорій Квітка-Основ'яненко");
+            $valce = new Book("\"Valse mélancolique\"", 545, "", "/pages/images/shop_list/u81.svg", "Ольга Кобилянська");
 
-      <!-- Unnamed (Rectangle) -->
-      <div id="u54" class="ax_default button">
-        <img id="u54_img" class="img " src="/pages/images/shop_list/u54.svg"/>
-        <div id="u54_text" class="text ">
-          <p><span>Fluent<br>Ukrainian</span></p>
-        </div>
-      </div>
+            $book_list = array($romantyka, $kaydasheva_simya, $tygrolovy, $trysta_poeziy, $konotopska_vidma, $valce);
 
-      <!-- Logo icon -->
-      <div id="u55" class="ax_default image">
-        <img id="u55_img" class="img " src="/pages/images/shop_list/u55.png"/>
-        <div id="u55_text" class="text " style="display:none; visibility: hidden">
-          <p></p>
-        </div>
-      </div>
+            $default_left_offset = 100;
+            $default_top_offset = 220;
 
-      <!-- Cart menu option -->
-      <div id="u89" class="ax_default icon">
-        <img id="u89_img" class="img " src="/pages/images/shop_list/u89.svg"/>
-        <div id="u89_text" class="text " style="display:none; visibility: hidden">
-          <p></p>
+            $left_offset = $default_left_offset;
+            $top_offset = $default_top_offset;
+            for ($i = 1; $i <= count($book_list); $i++) {
+              $book_title = $book_list[$i-1]->get_title();
+              $book_author = $book_list[$i-1]->get_author();
+              $book_price = $book_list[$i-1]->get_price();
+              $book_image_uri = $book_list[$i-1]->get_image_URI();
+              $offset = "top:" . $top_offset . "px;left:" . $left_offset . "px;";
+              include('book-block.php');
+              if ($i % 3 != 0) {
+                $left_offset += 430;
+              }
+              else {
+                $left_offset = $default_left_offset;
+                $top_offset += 580;
+              }
+            }
+      ?>
+
+      <footer>
+        <!-- Footer background -->
+        <div id="u21" class="ax_default box_2">
+          <div id="u21_div" class=""></div>
+          <div id="u21_text" class="text " style="display:none; visibility: hidden">
+            <p></p>
+          </div>
         </div>
-      </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u22" class="ax_default heading_2">
+          <div id="u22_div" class=""></div>
+          <div id="u22_text" class="text ">
+            <p><span>Про FluentUkrainian</span></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u23" class="ax_default heading_2">
+          <div id="u23_div" class=""></div>
+          <div id="u23_text" class="text ">
+            <p><span>Соціальні мережі</span></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u24" class="ax_default heading_2">
+          <div id="u24_div" class=""></div>
+          <div id="u24_text" class="text ">
+            <p><span>Застосунок</span></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Line) -->
+        <div id="u25" class="ax_default line1">
+          <img id="u25_img" class="img " src="/pages/images/shop_list/u25.svg"/>
+          <div id="u25_text" class="text " style="display:none; visibility: hidden">
+            <p></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u26" class="ax_default heading_2">
+          <div id="u26_div" class=""></div>
+          <div id="u26_text" class="text ">
+            <p><span>Підтримка</span></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u27" class="ax_default label">
+          <div id="u27_div" class=""></div>
+          <div id="u27_text" class="text ">
+            <p><span>© 2024 FluentUkrainian. Всі права захищені.</span></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Shape) -->
+        <div id="u28" class="ax_default icon">
+          <img id="u28_img" class="img " src="/pages/images/shop_list/u28.svg"/>
+          <div id="u28_text" class="text " style="display:none; visibility: hidden">
+            <p></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Shape) -->
+        <div id="u29" class="ax_default icon">
+          <img id="u29_img" class="img " src="/pages/images/shop_list/u29.svg"/>
+          <div id="u29_text" class="text " style="display:none; visibility: hidden">
+            <p></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Shape) -->
+        <div id="u30" class="ax_default icon">
+          <img id="u30_img" class="img " src="/pages/images/shop_list/u30.svg"/>
+          <div id="u30_text" class="text " style="display:none; visibility: hidden">
+            <p></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u31" class="ax_default label">
+          <div id="u31_div" class=""></div>
+          <div id="u31_text" class="text ">
+            <p><span>Facebook</span></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u32" class="ax_default label">
+          <div id="u32_div" class=""></div>
+          <div id="u32_text" class="text ">
+            <p><span>Instagram</span></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u33" class="ax_default label">
+          <div id="u33_div" class=""></div>
+          <div id="u33_text" class="text ">
+            <p><span>Telegram</span></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u34" class="ax_default label">
+          <div id="u34_div" class=""></div>
+          <div id="u34_text" class="text ">
+            <p><span>Освітній проєкт</span></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u35" class="ax_default label">
+          <div id="u35_div" class=""></div>
+          <div id="u35_text" class="text ">
+            <p><span>Підтримка</span></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u36" class="ax_default label">
+          <div id="u36_div" class=""></div>
+          <div id="u36_text" class="text ">
+            <p><span>Навчальні матеріали</span></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u37" class="ax_default label">
+          <div id="u37_div" class=""></div>
+          <div id="u37_text" class="text ">
+            <p><span>Питання та відповіді</span></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u38" class="ax_default label">
+          <div id="u38_div" class=""></div>
+          <div id="u38_text" class="text ">
+            <p><span>Детальніше</span></p>
+          </div>
+        </div>
+
+        <!-- Unnamed (Rectangle) -->
+        <div id="u39" class="ax_default label">
+          <div id="u39_div" class=""></div>
+          <div id="u39_text" class="text ">
+            <p><span>Викладачі</span></p>
+          </div>
+        </div>
+      </footer>
     </div>
     <script src="/pages/resources/scripts/axure/ios.js"></script>
   </body>
