027aa327717001cdab6a03619b580e7b026d9edc
annalysenk0 <129483819+annalysenk0@users.noreply.github.com>
2024-05-11 23:30:18 +0000

    add attendance_counter.php, homepage.php, change homepage.html

---
+++ models/attendance_counter.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['counted'])) {
+    $agent = $_SERVER['HTTP_USER_AGENT'];
+    $uri = $_SERVER['REQUEST_URI'];
+    $user = $_SERVER['PHP_AUTH_USER'];
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $ref = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "Ні";
+    $dtime = date('r');
+    
+    $entry_line = "$dtime - IP: $ip | Agent: $agent | URL: $uri | Referrer: $ref | Username: $user" . PHP_EOL;
+
+    $fp = fopen("logs.txt", "a");
+    fwrite($fp, $entry_line);
+    fclose($fp);
+
+    $_SESSION['counted'] = true; // Встановлюємо прапорець counted в сесії, щоб позначити, що запис був вже зроблений
+}
+?>
--- views/homepage.php
+++ views/homepage.php
@@ -50,7 +50,7 @@
   <body>
     
     <div id="base" class="">
-
+    <?php include_once("attendance_counter.php"); ?>
       <!-- Unnamed (Image) -->
       <div id="u0" class="ax_default image">
         <img id="u0_img" class="img " src="images/homepage/u0.png"/>
