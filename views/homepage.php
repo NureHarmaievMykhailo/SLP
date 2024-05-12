8824b33ad03e931a0fb0b3e6b08b202b8c0e9964
annalysenk0 <129483819+annalysenk0@users.noreply.github.com>
2024-05-12 16:29:06 +0000

    changed homepage.php

--- views/homepage.php
+++ views/homepage.php
@@ -5,56 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
   
-    <link href="resources/css/axure_rp_page.css" type="text/css" rel="stylesheet"/>
-    <link href="data/styles.css" type="text/css" rel="stylesheet"/>
-    <link href="files/homepage/styles.css" type="text/css" rel="stylesheet"/>
-    <script src="resources/scripts/jquery-3.7.1.min.js"></script>
-    <script src="resources/scripts/axure/axQuery.js"></script>
-    <script src="resources/scripts/axure/globals.js"></script>
-    <script src="resources/scripts/axutils.js"></script>
-    <script src="resources/scripts/axure/annotation.js"></script>
-    <script src="resources/scripts/axure/axQuery.std.js"></script>
-    <script src="resources/scripts/axure/doc.js"></script>
-    <script src="resources/scripts/messagecenter.js"></script>
-    <script src="resources/scripts/axure/events.js"></script>
-    <script src="resources/scripts/axure/recording.js"></script>
-    <script src="resources/scripts/axure/action.js"></script>
-    <script src="resources/scripts/axure/expr.js"></script>
-    <script src="resources/scripts/axure/geometry.js"></script>
-    <script src="resources/scripts/axure/flyout.js"></script>
-    <script src="resources/scripts/axure/model.js"></script>
-    <script src="resources/scripts/axure/repeater.js"></script>
-    <script src="resources/scripts/axure/sto.js"></script>
-    <script src="resources/scripts/axure/utils.temp.js"></script>
-    <script src="resources/scripts/axure/variables.js"></script>
-    <script src="resources/scripts/axure/drag.js"></script>
-    <script src="resources/scripts/axure/move.js"></script>
-    <script src="resources/scripts/axure/visibility.js"></script>
-    <script src="resources/scripts/axure/style.js"></script>
-    <script src="resources/scripts/axure/adaptive.js"></script>
-    <script src="resources/scripts/axure/tree.js"></script>
-    <script src="resources/scripts/axure/init.temp.js"></script>
-    <script src="resources/scripts/axure/legacy.js"></script>
-    <script src="resources/scripts/axure/viewer.js"></script>
-    <script src="resources/scripts/axure/math.js"></script>
-    <script src="resources/scripts/axure/jquery.nicescroll.min.js"></script>
-    <script src="data/document.js"></script>
-    <script src="files/homepage/data.js"></script>
+    <link href="../pages/resources/css/axure_rp_page.css" type="text/css" rel="stylesheet"/>
+    <link href=".../pages/data/styles.css" type="text/css" rel="stylesheet"/>
+    <link href="../pages/files/homepage/styles.css" type="text/css" rel="stylesheet"/>
+    <script src="../data/document.js"></script>
+    <script src="../pages/files/homepage/data.js"></script>
     <script type="text/javascript">
-      $axure.utils.getTransparentGifPath = function() { return 'resources/images/transparent.gif'; };
-      $axure.utils.getOtherPath = function() { return 'resources/Other.html'; };
-      $axure.utils.getReloadPath = function() { return 'resources/reload.html'; };
+      $axure.utils.getTransparentGifPath = function() { return '../pages/resources/images/transparent.gif'; };
+      $axure.utils.getOtherPath = function() { return '../pages/resources/Other.html'; };
+      $axure.utils.getReloadPath = function() { return '../pages/resources/reload.html'; };
     </script>
     
   </head>
   <body>
     
     <div id="base" class="">
-    <?php include_once("attendance_counter.php"); ?>
-    <?php include_once("certain_user.php"); ?>
+    
+    <!--  include_once("attendance_counter.php"); ?> 
+     include_once("certain_user.php"); ?>-->
       <!-- Unnamed (Image) -->
       <div id="u0" class="ax_default image">
-        <img id="u0_img" class="img " src="images/homepage/u0.png"/>
+        <img id="u0_img" class="img " src="../pages/images/homepage/u0.png"/>
         <div id="u0_text" class="text " style="display:none; visibility: hidden">
           <p></p>
         </div>
@@ -86,7 +57,7 @@
 
       <!-- Unnamed (Line) -->
       <div id="u4" class="ax_default line2">
-        <img id="u4_img" class="img " src="images/homepage/u4.svg"/>
+        <img id="u4_img" class="img " src="../pages/images/homepage/u4.svg"/>
         <div id="u4_text" class="text " style="display:none; visibility: hidden">
           <p></p>
         </div>
@@ -110,7 +81,7 @@
 
       <!-- Unnamed (Image) -->
       <div id="u7" class="ax_default image">
-        <img id="u7_img" class="img " src="images/homepage/u7.png"/>
+        <img id="u7_img" class="img " src="../pages/images/homepage/u7.png"/>
         <div id="u7_text" class="text " style="display:none; visibility: hidden">
           <p></p>
         </div>
@@ -190,7 +161,7 @@
 
       <!-- Unnamed (Line) -->
       <div id="u17" class="ax_default line2">
-        <img id="u17_img" class="img " src="images/homepage/u4.svg"/>
+        <img id="u17_img" class="img " src="../pages/images/homepage/u4.svg"/>
         <div id="u17_text" class="text " style="display:none; visibility: hidden">
           <p></p>
         </div>
@@ -214,7 +185,7 @@
 
       <!-- Unnamed (Image) -->
       <div id="u20" class="ax_default image">
-        <img id="u20_img" class="img " src="images/homepage/u7.png"/>
+        <img id="u20_img" class="img " src="../pages/images/homepage/u7.png"/>
         <div id="u20_text" class="text " style="display:none; visibility: hidden">
           <p></p>
         </div>
@@ -230,14 +201,14 @@
 
       <!-- Unnamed (Menu) -->
       <div id="u22" class="ax_default">
-        <img id="u22_menu" class="img " src="images/homepage/u22_menu.png" alt="u22_menu"/>
+        <img id="u22_menu" class="img " src="../pages/images/homepage/u22_menu.png" alt="u22_menu"/>
 
         <!-- Unnamed (Table) -->
         <div id="u23" class="ax_default">
 
           <!-- Unnamed (Menu Item) -->
           <div id="u24" class="ax_default menu_item">
-            <img id="u24_img" class="img " src="images/homepage/u24.png"/>
+            <img id="u24_img" class="img " src="../pages/images/homepage/u24.png"/>
             <div id="u24_text" class="text " style="display:none; visibility: hidden">
               <p></p>
             </div>
@@ -245,7 +216,7 @@
 
           <!-- Unnamed (Menu Item) -->
           <div id="u25" class="ax_default menu_item">
-            <img id="u25_img" class="img " src="images/homepage/u25.png"/>
+            <img id="u25_img" class="img " src="../pages/images/homepage/u25.png"/>
             <div id="u25_text" class="text ">
               <p><span>Головна сторінка</span></p>
             </div>
@@ -253,7 +224,7 @@
 
           <!-- Unnamed (Menu Item) -->
           <div id="u26" class="ax_default menu_item">
-            <img id="u26_img" class="img " src="images/homepage/u26.png"/>
+            <img id="u26_img" class="img " src="../pages/images/homepage/u26.png"/>
             <div id="u26_text" class="text ">
               <p><span>Навчальні матеріали</span></p>
             </div>
@@ -261,7 +232,7 @@
 
           <!-- Unnamed (Menu Item) -->
           <div id="u27" class="ax_default menu_item">
-            <img id="u27_img" class="img " src="images/homepage/u27.png"/>
+            <img id="u27_img" class="img " src="../pages/images/homepage/u27.png"/>
             <div id="u27_text" class="text ">
               <p><span>Викладачі</span></p>
             </div>
@@ -269,7 +240,7 @@
 
           <!-- Unnamed (Menu Item) -->
           <div id="u28" class="ax_default menu_item">
-            <img id="u28_img" class="img " src="images/homepage/u28.png"/>
+            <img id="u28_img" class="img " src="../pages/images/homepage/u28.png"/>
             <div id="u28_text" class="text ">
               <p><span>Мобільний застосунок</span></p>
             </div>
@@ -277,7 +248,7 @@
 
           <!-- Unnamed (Menu Item) -->
           <div id="u29" class="ax_default menu_item">
-            <img id="u29_img" class="img " src="images/homepage/u29.png"/>
+            <img id="u29_img" class="img " src="../pages/images/homepage/u29.png"/>
             <div id="u29_text" class="text " style="display:none; visibility: hidden">
               <p></p>
             </div>
@@ -287,7 +258,7 @@
 
       <!-- Unnamed (Rectangle) -->
       <div id="u30" class="ax_default button">
-        <img id="u30_img" class="img " src="images/homepage/u30.svg"/>
+        <img id="u30_img" class="img " src="../pages/images/homepage/u30.svg"/>
         <div id="u30_text" class="text ">
           <p><span>Fluent<br>Ukrainian</span></p>
         </div>
@@ -295,7 +266,7 @@
 
       <!-- Unnamed (Image) -->
       <div id="u31" class="ax_default image">
-        <img id="u31_img" class="img " src="images/homepage/u31.png"/>
+        <img id="u31_img" class="img " src="../pages/images/homepage/u31.png"/>
         <div id="u31_text" class="text " style="display:none; visibility: hidden">
           <p></p>
         </div>
@@ -335,7 +306,7 @@
 
       <!-- Unnamed (Image) -->
       <div id="u37" class="ax_default image">
-        <img id="u37_img" class="img " src="images/homepage/u37.svg"/>
+        <img id="u37_img" class="img " src="../pages/images/homepage/u37.svg"/>
         <div id="u37_text" class="text " style="display:none; visibility: hidden">
           <p></p>
         </div>
@@ -407,7 +378,7 @@
 
       <!-- Unnamed (Image) -->
       <div id="u47" class="ax_default image">
-        <img id="u47_img" class="img " src="images/homepage/u47.svg"/>
+        <img id="u47_img" class="img " src="../pages/images/homepage/u47.svg"/>
         <div id="u47_text" class="text " style="display:none; visibility: hidden">
           <p></p>
         </div>
@@ -471,7 +442,7 @@
 
       <!-- Unnamed (Image) -->
       <div id="u56" class="ax_default image">
-        <img id="u56_img" class="img " src="images/homepage/u56.svg"/>
+        <img id="u56_img" class="img " src="../pages/images/homepage/u56.svg"/>
         <div id="u56_text" class="text " style="display:none; visibility: hidden">
           <p></p>
         </div>
@@ -543,7 +514,7 @@
 
       <!-- Unnamed (Line) -->
       <div id="u65" class="ax_default line1">
-        <img id="u65_img" class="img " src="images/homepage/u65.svg"/>
+        <img id="u65_img" class="img " src="../pages/images/homepage/u65.svg"/>
         <div id="u65_text" class="text " style="display:none; visibility: hidden">
           <p></p>
         </div>
@@ -567,7 +538,7 @@
 
       <!-- Unnamed (Shape) -->
       <div id="u68" class="ax_default icon">
-        <img id="u68_img" class="img " src="images/homepage/u68.svg"/>
+        <img id="u68_img" class="img " src="../pages/images/homepage/u68.svg"/>
         <div id="u68_text" class="text " style="display:none; visibility: hidden">
           <p></p>
         </div>
@@ -575,7 +546,7 @@
 
       <!-- Unnamed (Shape) -->
       <div id="u69" class="ax_default icon">
-        <img id="u69_img" class="img " src="images/homepage/u69.svg"/>
+        <img id="u69_img" class="img " src="../pages/images/homepage/u69.svg"/>
         <div id="u69_text" class="text " style="display:none; visibility: hidden">
           <p></p>
         </div>
@@ -583,7 +554,7 @@
 
       <!-- Unnamed (Shape) -->
       <div id="u70" class="ax_default icon">
-        <img id="u70_img" class="img " src="images/homepage/u70.svg"/>
+        <img id="u70_img" class="img " src="../pages/images/homepage/u70.svg"/>
         <div id="u70_text" class="text " style="display:none; visibility: hidden">
           <p></p>
         </div>
@@ -677,6 +648,6 @@
         </div>
       </div>
     </div>
-    <script src="resources/scripts/axure/ios.js"></script>
+    <script src="../pages/resources/scripts/axure/ios.js"></script>
   </body>
 </html>
