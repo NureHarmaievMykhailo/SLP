57c460427f295c03cf1629a3337cd5bd9e32cc2e
Sayan <garmsayan@gmail.com>
2024-05-17 02:29:05 +0300

    Added sign_up and login pages

---
+++ views/login.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Sign up</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link href="../pages/resources/css/axure_rp_page.css" type="text/css" rel="stylesheet" />
+    <link href=".../pages/data/styles.css" type="text/css" rel="stylesheet" />
+    <link href="../pages/files/login/styles.css" type="text/css" rel="stylesheet" />
+</head>
+<boby>
+    <div id="base" class="">
+        <?php
+        session_start();
+        include('header.html');
+        ?>
+        <div id="base" class="">
+
+            <!-- Unnamed (Image) -->
+            <div id="u105" class="ax_default image">
+                <img id="u105_img" class="img " src="images/homepage/u0.png" />
+                <div id="u105_text" class="text " style="display:none; visibility: hidden">
+                    <p></p>
+                </div>
+            </div>
+
+            <!-- Unnamed (Image) -->
+            <div id="u106" class="ax_default image">
+                <img id="u106_img" class="img " src="images/homepage/u31.png" />
+                <div id="u106_text" class="text " style="display:none; visibility: hidden">
+                    <p></p>
+                </div>
+            </div>
+
+            <!-- Unnamed (Rectangle) -->
+            <div id="u107" class="ax_default box_1">
+                <img id="u107_img" class="img " src="images/login/u107.svg" />
+                <div id="u107_text" class="text " style="display:none; visibility: hidden">
+                    <p></p>
+                </div>
+            </div>
+
+            <!-- Unnamed (Rectangle) -->
+            <div id="u108" class="ax_default heading_1">
+                <div id="u108_div" class=""></div>
+                <div id="u108_text" class="text ">
+                    <p><span>Увійти</span></p>
+                </div>
+            </div>
+
+            <!-- Unnamed (Image) -->
+            <div id="u109" class="ax_default image">
+                <img id="u109_img" class="img " src="images/login/u109.png" />
+                <div id="u109_text" class="text " style="display:none; visibility: hidden">
+                    <p></p>
+                </div>
+            </div>
+
+            <!-- Unnamed (Image) -->
+            <div id="u110" class="ax_default image">
+                <img id="u110_img" class="img " src="images/login/u110.png" />
+                <div id="u110_text" class="text " style="display:none; visibility: hidden">
+                    <p></p>
+                </div>
+            </div>
+
+            <!-- Unnamed (Rectangle) -->
+            <div id="u111" class="ax_default label">
+                <div id="u111_div" class=""></div>
+                <div id="u111_text" class="text ">
+                    <p><span>Забули пароль?</span></p>
+                </div>
+            </div>
+
+            <!-- Unnamed (Rectangle) -->
+            <div id="u112" class="ax_default button">
+                <div id="u112_div" class=""></div>
+                <div id="u112_text" class="text ">
+                    <p><span>Увійти</span></p>
+                </div>
+            </div>
+
+            <!-- Unnamed (Rectangle) -->
+            <div id="u113" class="ax_default button">
+                <div id="u113_div" class=""></div>
+                <div id="u113_text" class="text ">
+                    <p><span>&nbsp;&nbsp; &nbsp;&nbsp; Увійти з допомогою Google</span></p>
+                </div>
+            </div>
+
+            <!-- Unnamed (Image) -->
+            <div id="u114" class="ax_default image">
+                <img id="u114_img" class="img " src="images/sign_up/u89.png" />
+                <div id="u114_text" class="text " style="display:none; visibility: hidden">
+                    <p></p>
+                </div>
+            </div>
+
+            <!-- Unnamed (Rectangle) -->
+            <div id="u115" class="ax_default label">
+                <div id="u115_div" class=""></div>
+                <div id="u115_text" class="text ">
+                    <p><span>Не маєте акаунт? </span></p>
+                </div>
+            </div>
+
+            <!-- Unnamed (Rectangle) -->
+            <div id="u116" class="ax_default label">
+                <div id="u116_div" class=""></div>
+                <div id="u116_text" class="text ">
+                    <p><span>Зареєструйтеся</span></p>
+                </div>
+            </div>
+
+            <!-- Name (Text Field) -->
+            <div id="u117" class="ax_default text_field">
+                <img id="u117_img" class="img " src="images/login/u117.svg" />
+                <input id="name" type="text" value="&nbsp; Ім'я користувача" class="u117_input" />
+            </div>
+
+            <!-- Password (Text Field) -->
+            <div id="u118" class="ax_default text_field">
+                <img id="u118_img" class="img " src="images/login/u117.svg" />
+                <input id="password" type="text" value="&nbsp; Пароль" class="u118_input" />
+            </div>
+        </div>
+
+
+
+</boby>
+
+</html>
