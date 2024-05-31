750e7793dc1987a51059920bd5a939e526938077
Оксана Афанасьєва <afoksana31@gmail.com>
2024-05-31 20:50:44 +0200

    Added page "user_control_panel" and admin-header

---
+++ views/admin-header.php
@@ -0,0 +1,8 @@
+<link href="../public/moderator-header.css" type="text/css" rel="stylesheet" />
+<nav class="mod_navbar">
+    <ul class="mod_nav_list">
+        <li class="mod_nav_item noselect"><a href="user_control_panel">User overview</a></li>
+        <li class="mod_nav_item noselect"><a href="">Moderator overview</a></li>
+        <li class="mod_nav_item noselect"><a href="">Add new moderator</a></li>
+    </ul>
+</nav>
